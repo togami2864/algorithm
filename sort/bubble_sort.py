0bb0f04a7c3febc388488bb70a9531eb2e5b53fe
togami2864 <tuabobo123@gmail.com>
2021-12-17 17:09:02 +0900

    fix bubble sort

--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -1,7 +1,6 @@
 def bubble_sort(arr):
-    for i in range(0, len(arr)-1):
-        for j in reversed(range(i, len(arr)-1)):
-            print(j)
+    for i in range(len(arr)):
+        for j in range(len(arr) - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
